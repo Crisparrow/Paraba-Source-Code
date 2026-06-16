989dc593966c73ab867f7bfea4902fa49e9f512c
Crisparrow <cristhianmejia581@gmail.com>
2026-06-16 09:44:14 -0400

    Baja proyecto a net8 para Visual Studio 2022

--- Paraba.UI/Paraba.UI/Program.cs
+++ Paraba.UI/Paraba.UI/Program.cs
@@ -33,17 +33,15 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
